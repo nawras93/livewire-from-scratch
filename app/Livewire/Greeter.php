a4d6cb6657aecf7e1808321afef0c1f747f4bc4a
ISMAIL MAHMOUD <imahmoud@moi.gov.qa>
2025-01-19 10:11:17 +0300

    Lesson 1

---
+++ app/Livewire/Greeter.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Greeter extends Component
+{
+    public $name = 'John';
+    public function render()
+    {
+        return view('livewire.greeter');
+    }
+}
