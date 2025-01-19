c382bc2c25228994144f19eacdd1449bdd1bbbf7
ISMAIL MAHMOUD <imahmoud@moi.gov.qa>
2025-01-19 10:32:24 +0300

    Lesson 2

--- app/Livewire/Greeter.php
+++ app/Livewire/Greeter.php
@@ -7,6 +7,11 @@ use Livewire\Component;
 class Greeter extends Component
 {
     public $name = 'John';
+
+    public function changeName($newName)
+    {
+        return $this->name = $newName;
+    }
     public function render()
     {
         return view('livewire.greeter');
--- resources/views/livewire/greeter.blade.php
+++ resources/views/livewire/greeter.blade.php
@@ -1,3 +1,33 @@
 <div>
-    Hello {{ $name }}
+    <div>
+        Hello {{ $name }}!
+    </div>
+
+{{--    <div class="mt-4">--}}
+{{--        <input--}}
+{{--            id="newName"--}}
+{{--            class="block w-full p-4 border rounded-md bg-gray-700 text-white" type="text">--}}
+{{--    </div>--}}
+{{--    <div class="mt-4">--}}
+{{--        <button wire:click="changeName(document.querySelector('#newName').value)" class="text-white font-medium rounded-xl px-4 bg-blue-500">--}}
+{{--            Greet--}}
+{{--        </button>--}}
+{{--    </div>--}}
+
+    <form
+        wire:submit="changeName(document.querySelector('#newName').value)"
+    >
+        <div class="mt-4">
+            <input
+                id="newName"
+                class="block w-full p-4 border rounded-md bg-gray-700 text-white" type="text">
+        </div>
+        <div class="mt-4">
+            <button
+                type="submit"
+                class="text-white font-medium rounded-xl px-4 bg-blue-500">
+                Greet
+            </button>
+        </div>
+    </form>
 </div>
